ea8eeac87bc059b4fb049541a0b66ff68ec33439
Megan Warnock <megandwarnock@gmail.com>
2017-06-14 11:17:21 -0700

    set up initial file structure

---
+++ android/src/com/megandwarnock/pdxbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.megandwarnock.pdxbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.megandwarnock.pdxbird.PdxBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PdxBird(), config);
+	}
+}
