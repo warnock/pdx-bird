cdb7f6881321734b4b3fb9a8af20984ed370023d
Megan Warnock <megandwarnock@gmail.com>
2017-06-14 13:26:29 -0700

    add velocity and gravity to bird sprite

--- core/src/com/megandwarnock/pdxbird/PdxBird.java
+++ core/src/com/megandwarnock/pdxbird/PdxBird.java
@@ -12,19 +12,43 @@ public class PdxBird extends ApplicationAdapter {
 
 	Texture[] birds;
 	int flapState = 0;
+	float birdY = 0;
+	float velocity = 0;
+
+	int gameState = 0;
+	float gravity = 2;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+
 		birds = new Texture[3];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird.png");
 		birds[2] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight() / 2 - birds[flapState].getHeight() /2;
 	}
 
 	@Override
 	public void render () {
+
+		if (gameState != 0) {
+
+			if(Gdx.input.justTouched()) {
+				velocity = -30;
+			}
+
+			velocity = velocity + gravity;
+			birdY -= velocity;
+
+		} else {
+
+			if(Gdx.input.justTouched()) {
+				gameState = 1;
+
+			}
+		}
 
 		if (flapState == 0) {
 			flapState = 1;
@@ -36,7 +60,7 @@ public class PdxBird extends ApplicationAdapter {
 
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, Gdx.graphics.getHeight() / 2 - birds[flapState].getHeight() / 2);
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
 		batch.end();
 	}
 }
